1e0299bf8c4aa4605c7ce9efc9cc8c3e2f43ef84
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-01 05:15:04 -0300

    Update data.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,4 +1,4 @@
-const api = require('../../api');
+const api = require('../../../api');
 const SMA = require('technicalindicators').SMA;
 const StochasticRSI = require('technicalindicators').StochasticRSI;
 
