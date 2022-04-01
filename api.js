eb440665f3f47de227e72b52f99868560f499831
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-01 04:20:20 -0300

    add files

---
+++ api.js
@@ -0,0 +1,134 @@
+const axios = require('axios');
+const queryString = require('querystring');
+ 
+async function publicCall(path, data, method = 'GET', headers = {}) {
+    try {
+        const qs = data ? `?${queryString.stringify(data)}` : '';
+        const result = await axios({
+            method,
+            url: `${process.env.API_URL}${path}${qs}`
+        });
+        return result;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const crypto = require('crypto');
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY;
+const apiUrl = process.env.API_URL;
+const symbol = process.env.SYMBOL;
+ 
+async function privateCall(path, timestamp, data = {}, method = 'GET') {
+    if (!apiKey || !apiSecret){
+        throw new Error('Preencha corretamente sua API KEY e SECRET KEY');
+    }
+    const type = "FUTURES";
+    //const timestamp = (Date.now())-1000;
+    const recvWindow = 60000;//máximo permitido, default 5000
+    
+    const signature = crypto
+        .createHmac('sha256', apiSecret)
+        .update(`${queryString.stringify({ ...data, type, timestamp, recvWindow })}`)
+        .digest('hex');
+ 
+    const newData = { ...data, type, timestamp, recvWindow, signature };
+    const qs = `?${queryString.stringify(newData)}`;
+ 
+    try {
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey }
+        });
+        return result.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+ 
+async function time() {
+    return publicCall('/api/v3/time');
+}
+ 
+async function depth(symbol = 'BTCBRL', limit = 5) {
+    return publicCall('/api/v3/depth', { symbol, limit });
+}
+
+async function exchangeInfo(symbol) {
+    const result = await publicCall('/api/v3/exchangeInfo');
+    return symbol ? result.symbols.find(s => s.symbol === symbol) : result.symbols;
+}
+ 
+async function accountInfo(){
+    return privateCall('/v3/account');
+}
+
+async function accountSnapshot(timestamp){
+
+    return privateCall('/sapi/v1/accountSnapshot',timestamp);
+}
+
+async function klines(interval){
+    const limit = 100;
+    return publicCall('/api/v3/klines',{symbol, interval, limit});
+}
+
+
+
+
+
+module.exports = { time, depth, exchangeInfo, accountSnapshot, klines }
+
+/*
+GET /sapi/v1/accountSnapshot (HMAC SHA256)
+
+Weight(IP): 2400
+
+Parameters:
+
+Name	Type	Mandatory	Description
+type	STRING	YES	"SPOT", "MARGIN", "FUTURES"
+startTime	LONG	NO	
+endTime	LONG	NO	
+limit	INT	NO	min 7, max 30, default 7
+recvWindow	LONG	NO	
+timestamp	LONG	YES
+
+
+
+GET /api/v3/klines
+
+Kline/candlestick bars for a symbol.
+Klines are uniquely identified by their open time.
+
+Weight(IP): 1
+
+Parameters:
+
+Name	Type	Mandatory	Description
+symbol	STRING	YES	
+interval	ENUM	YES	
+startTime	LONG	NO	
+endTime	LONG	NO	
+limit	INT	NO
+
+[
+  [
+    1499040000000,      // Open time
+    "0.01634790",       // Open
+    "0.80000000",       // High
+    "0.01575800",       // Low
+    "0.01577100",       // Close
+    "148976.11427815",  // Volume
+    1499644799999,      // Close time
+    "2434.19055334",    // Quote asset volume
+    308,                // Number of trades
+    "1756.87402397",    // Taker buy base asset volume
+    "28.46694368",      // Taker buy quote asset volume
+    "17928899.62484339" // Ignore.
+  ]
+]
+
+*/
--- pages/api/data.js
+++ pages/api/data.js
@@ -1,5 +1,122 @@
-function tempo(request, response){
+function data(request, response){
     const dynamicDate = new Date();
+
+
+    marketData = null;
+    timestampArr = [];
+    dateArr = [];
+    openArr = [];
+    closeArr = [];
+    highArr = [];
+    lowArr = [];
+    volArr = [];
+
+    const timeApi = await api.time();
+    console.log(`serverTime: ${timeApi.data.serverTime}`);
+    dados.serverTimestamp = await timeApi.data.serverTime;
+
+    
+    //const ts = timeApi.serverTime;
+
+    const carteira = await api.accountSnapshot(data.serverTimestamp);
+    const coin = carteira.snapshotVos[0].data.assets.filter(b => b.asset === 'USDT'); // || b.asset === 'USDT');
+    data.marginBalance = coin[0].marginBalance;
+    console.log(`TEST:coins:  ${JSON.stringify(coin[0].marginBalance)}`);
+
+
+    //console.log(await api.time());
+    result = await api.time();
+    console.log(`serverTime: ${result.data.serverTime}`);
+    ts = result.serverTime;
+
+    result = await api.klines("15m");
+    console.log(`klines0: ${JSON.stringify(result.data[0])}`);
+    console.log('');
+    console.log(`klines00: ${JSON.stringify(result.data[result.data.length-1])}`);
+  
+    for (let i = 0; i < result.data.length-1; i++) {
+        criarObj(result.data[i]);
+    }
+
+    //invertido    
+    for (let i = 40; i > 0; i--) {
+        //criarObj(result.data[i]);
+    }    
+
+    marketData = { date: dateArr, timestamp: timestampArr, open: openArr, close: closeArr, high: highArr, low: lowArr, volume: volArr };
+    console.log('');
+    console.log(`marketData.date(últimoCandle): ${JSON.stringify(marketData.date[result.data.length-2])}`);
+
+    /*
+    //console.log('SMA: ');
+    //console.log(SMA.calculate({period : 5, values : [1,2,3,4,5,6,7,8,9]}));
+    console.log('');
+    console.log('<<--   StochasticRSI   -->>');
+    console.log(StochasticRSI.calculate({values: marketData.close,
+        rsiPeriod: 14,
+        stochasticPeriod: 14,
+        kPeriod: 3,
+        dPeriod: 3
+    }));
+    */
+
+    var stochRsi = StochasticRSI.calculate({values: marketData.close,
+        rsiPeriod: 14,
+        stochasticPeriod: 14,
+        kPeriod: 3,
+        dPeriod: 3
+    });
+
+    console.log('');
+    console.log('stochRsiData');
+    console.log(stochRsi[stochRsi.length-1]);
+
+    //console.log(await api.exchangeInfo());
+/*
+    console.log('Mercado');
+
+    const mercado = await api.depth('BNBBUSD');
+    console.log(mercado.bids.length ? `Compra: ${mercado.bids[0][0]}` : 'Sem Compras');
+    console.log(mercado.asks.length ? `Venda: ${mercado.asks[0][0]}` : 'Sem Vendas');
+
+    console.log(await api.depth(symbol));
+
+    let buy, sell;
+
+    const result = await api.depth(symbol);
+    if (result.bids && result.bids.length){
+        console.log(`HI BUY: ${result.bids[0][0]}`);
+        buy = parseFloat(result.bids[0][0]);
+    }
+
+    if (result.asks && result.asks.length){
+        console.log(`LOW SELL: ${result.asks[0][0]}`);
+        sell = parseFloat(result.asks[0][0]);
+    }
+
+    if(sell < 200000){
+        console.log('Hora de Comprar!!!')
+
+        
+    }else if(buy > 230000){
+        console.log('Hora de Vender!!!')
+    }else{
+        console.log('ESPERANDO!!!')
+    }
+
+    console.log('Carteira');
+
+    result = await api.accountSnapshot(ts);
+    console.log(result);
+    objStr = JSON.stringify(result.snapshotVos[0].data);
+    console.log(`TEST:snapshotVos: ${objStr}`);   
+
+    const carteira = await api.accountSnapshot(ts);
+    const coin = carteira.snapshotVos[0].data.assets.filter(b => b.asset === 'USDT'); // || b.asset === 'USDT');
+    
+    console.log(`TEST:coins:  ${JSON.stringify(coin[0].marginBalance)}`);
+    //coin:  [{"asset":"USDT","marginBalance":"0.02738226","walletBalance":"0.02738226"}]
+*/
 
     response.json({
         serverTimestamp: dynamicDate,
@@ -8,4 +125,32 @@ function tempo(request, response){
     //{"serverTimestamp":"1648712608125","marginBalance":"0.02738226"}
 }
 
-export default tempo;
+function criarObj(item){
+
+    let unix_timestamp = item[0]
+    //var date = new Date(item[0]);
+    //console.log(date.getTime())
+    //console.log(date)
+
+    var date = new Date(unix_timestamp);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    
+    var formattedTime = hours + ':' + minutes + ':' + seconds;
+    
+    //console.log(formattedTime);
+
+    dateArr.push(formattedTime);
+    //dateArr.push(date);
+    timestampArr.push(unix_timestamp);
+    openArr.push(item[1]);
+    closeArr.push(item[4]);
+    highArr.push(item[2]);
+    lowArr.push(item[3]);
+    volArr.push(item[5]);
+    //marketData = { open: item[1], close: item[4], high: item[2], low: item[3], volume: item[5] };
+
+}
+
+export default data;
