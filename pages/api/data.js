ad3525d3357de6cc8739a5f183c7d926d2953982
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-01 03:51:21 -0300

    Create data.js

---
+++ pages/api/data.js
@@ -0,0 +1,11 @@
+function tempo(request, response){
+    const dynamicDate = new Date();
+
+    response.json({
+        serverTimestamp: dynamicDate,
+        marginBalance: "0.02738226"
+    })
+    //{"serverTimestamp":"1648712608125","marginBalance":"0.02738226"}
+}
+
+export default tempo;
