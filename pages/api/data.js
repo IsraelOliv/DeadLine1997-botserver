b973ff9cb8d9b615a3b70ef0b5c50a06f747c8b1
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-10 04:52:19 -0300

    Update data.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -345,7 +345,57 @@ async function data(request, response){
         dPeriod: 3
     }));
     */
-    writeUserData();
+    
+    let objSendcalc = {
+
+        lastUpdate: lastUpdate,
+        balance: balance,
+        availableBalance: availableBalance,
+        marginBalance: marginBalance,
+        unrealizedProfit: unrealizedProfit,
+        serverTimestamp: timeApi.data.serverTime,
+        tick: marketData1m.close[marketData1m.close.length-1],
+        tickprev: marketData1m.close[marketData1m.close.length-2],
+
+        lastUpdtMarket1m: marketData1m.date[marketData1m.date.length-1],
+        stoch1m: stochRsi1m[stochRsi1m.length-1],
+        stoch1mprev: stochRsi1m[stochRsi1m.length-2],
+
+        lastUpdtMarket3m: marketData3m.date[marketData3m.date.length-1],
+        stoch3m: stochRsi3m[stochRsi3m.length-1],
+        stoch3mprev: stochRsi3m[stochRsi3m.length-2],
+
+        lastUpdtMarket5m: marketData5m.date[marketData5m.date.length-1],
+        stoch5m: stochRsi5m[stochRsi5m.length-1],
+        stoch5mprev: stochRsi5m[stochRsi5m.length-2],
+
+        lastUpdtMarket15m: marketData15m.date[marketData15m.date.length-1],
+        stoch15m: stochRsi15m[stochRsi15m.length-1],
+        stoch15mprev: stochRsi15m[stochRsi15m.length-2],
+
+        lastUpdtMarket30m: marketData30m.date[marketData30m.date.length-1],
+        stoch30m: stochRsi30m[stochRsi30m.length-1],
+        stoch30mprev: stochRsi30m[stochRsi30m.length-2],
+
+        lastUpdtMarket1h: marketData1h.date[marketData1h.date.length-1],
+        stoch1h: stochRsi1h[stochRsi1h.length-1],
+        stoch1hprev: stochRsi1h[stochRsi1h.length-2],
+
+        lastUpdtMarket4h: marketData4h.date[marketData4h.date.length-1],
+        stoch4h: stochRsi4h[stochRsi4h.length-1],
+        stoch4hprev: stochRsi4h[stochRsi4h.length-2],
+        
+        lastUpdtMarket1d: marketData1d.date[marketData1d.date.length-1],
+        stoch1d: stochRsi1d[stochRsi1d.length-1],
+        stoch1dprev: stochRsi1d[stochRsi1d.length-2],
+
+        lastUpdtMarket1w: marketData1w.date[marketData1w.date.length-1],
+        stoch1w: stochRsi1w[stochRsi1w.length-1],
+        stoch1wprev: stochRsi1w[stochRsi1w.length-2]    
+
+    };
+    
+    writeUserData(objSendcalc);
 
     //console.log(await api.exchangeInfo());
 /*
@@ -448,61 +498,14 @@ async function data(request, response){
     //{"serverTimestamp":"1648712608125","marginBalance":"0.02738226"}
 }
 
-function writeUserData() {
+function writeUserData(objSendcalc) {
 
     const app = initializeApp(firebaseConfig);
     const database = getDatabase(app);
     //const db = getDatabase();
     //set(ref(database, 'rsidata/' + userId), {
-    set(ref(database, 'rsidata'), {
-
-        lastUpdate: lastUpdate,
-        balance: balance,
-        availableBalance: availableBalance,
-        marginBalance: marginBalance,
-        unrealizedProfit: unrealizedProfit,
-        serverTimestamp: timeApi.data.serverTime,
-        tick: marketData1m.close[marketData1m.close.length-1],
-        tickprev: marketData1m.close[marketData1m.close.length-2],
-
-        lastUpdtMarket1m: marketData1m.date[marketData1m.date.length-1],
-        stoch1m: stochRsi1m[stochRsi1m.length-1],
-        stoch1mprev: stochRsi1m[stochRsi1m.length-2],
-
-        lastUpdtMarket3m: marketData3m.date[marketData3m.date.length-1],
-        stoch3m: stochRsi3m[stochRsi3m.length-1],
-        stoch3mprev: stochRsi3m[stochRsi3m.length-2],
-
-        lastUpdtMarket5m: marketData5m.date[marketData5m.date.length-1],
-        stoch5m: stochRsi5m[stochRsi5m.length-1],
-        stoch5mprev: stochRsi5m[stochRsi5m.length-2],
-
-        lastUpdtMarket15m: marketData15m.date[marketData15m.date.length-1],
-        stoch15m: stochRsi15m[stochRsi15m.length-1],
-        stoch15mprev: stochRsi15m[stochRsi15m.length-2],
-
-        lastUpdtMarket30m: marketData30m.date[marketData30m.date.length-1],
-        stoch30m: stochRsi30m[stochRsi30m.length-1],
-        stoch30mprev: stochRsi30m[stochRsi30m.length-2],
-
-        lastUpdtMarket1h: marketData1h.date[marketData1h.date.length-1],
-        stoch1h: stochRsi1h[stochRsi1h.length-1],
-        stoch1hprev: stochRsi1h[stochRsi1h.length-2],
-
-        lastUpdtMarket4h: marketData4h.date[marketData4h.date.length-1],
-        stoch4h: stochRsi4h[stochRsi4h.length-1],
-        stoch4hprev: stochRsi4h[stochRsi4h.length-2],
-        
-        lastUpdtMarket1d: marketData1d.date[marketData1d.date.length-1],
-        stoch1d: stochRsi1d[stochRsi1d.length-1],
-        stoch1dprev: stochRsi1d[stochRsi1d.length-2],
-
-        lastUpdtMarket1w: marketData1w.date[marketData1w.date.length-1],
-        stoch1w: stochRsi1w[stochRsi1w.length-1],
-        stoch1wprev: stochRsi1w[stochRsi1w.length-2]    
-    
-    });
-  }
+    set(ref(database, 'rsidata'), objSendcalc);
+}
 
 
 /*
