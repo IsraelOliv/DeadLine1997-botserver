17c1648c3c4f0103c3babfb61a911c2b20589ed0
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-10 08:55:49 -0300

    Update api.js

--- pages/api/api.js
+++ pages/api/api.js
@@ -136,6 +136,172 @@ module.exports = { time, depth, exchangeInfo, accountSnapshot, balance, accountF
 
 /*
 
+Current All Open Orders (USER_DATA)
+Response:
+
+[
+  {
+    "avgPrice": "0.00000",
+    "clientOrderId": "abc",
+    "cumQuote": "0",
+    "executedQty": "0",
+    "orderId": 1917641,
+    "origQty": "0.40",
+    "origType": "TRAILING_STOP_MARKET",
+    "price": "0",
+    "reduceOnly": false,
+    "side": "BUY",
+    "positionSide": "SHORT",
+    "status": "NEW",
+    "stopPrice": "9300",                // please ignore when order type is TRAILING_STOP_MARKET
+    "closePosition": false,   // if Close-All
+    "symbol": "BTCUSDT",
+    "time": 1579276756075,              // order time
+    "timeInForce": "GTC",
+    "type": "TRAILING_STOP_MARKET",
+    "activatePrice": "9020",            // activation price, only return with TRAILING_STOP_MARKET order
+    "priceRate": "0.3",                 // callback rate, only return with TRAILING_STOP_MARKET order
+    "updateTime": 1579276756075,        // update time
+    "workingType": "CONTRACT_PRICE",
+    "priceProtect": false            // if conditional order trigger is protected   
+  }
+]
+GET /fapi/v1/openOrders (HMAC SHA256)
+
+Get all open orders on a symbol. Careful when accessing this with no symbol.
+
+Weight: 1 for a single symbol; 40 when the symbol parameter is omitted
+
+Parameters:
+
+Name	Type	Mandatory	Description
+symbol	STRING	NO	
+recvWindow	LONG	NO	
+timestamp	LONG	YES	
+If the symbol is not sent, orders for all symbols will be returned in an array.
+
+
+
+All Orders (USER_DATA)
+Response:
+
+[
+  {
+    "avgPrice": "0.00000",
+    "clientOrderId": "abc",
+    "cumQuote": "0",
+    "executedQty": "0",
+    "orderId": 1917641,
+    "origQty": "0.40",
+    "origType": "TRAILING_STOP_MARKET",
+    "price": "0",
+    "reduceOnly": false,
+    "side": "BUY",
+    "positionSide": "SHORT",
+    "status": "NEW",
+    "stopPrice": "9300",                // please ignore when order type is TRAILING_STOP_MARKET
+    "closePosition": false,   // if Close-All
+    "symbol": "BTCUSDT",
+    "time": 1579276756075,              // order time
+    "timeInForce": "GTC",
+    "type": "TRAILING_STOP_MARKET",
+    "activatePrice": "9020",            // activation price, only return with TRAILING_STOP_MARKET order
+    "priceRate": "0.3",                 // callback rate, only return with TRAILING_STOP_MARKET order
+    "updateTime": 1579276756075,        // update time
+    "workingType": "CONTRACT_PRICE",
+    "priceProtect": false            // if conditional order trigger is protected   
+  }
+]
+GET /fapi/v1/allOrders (HMAC SHA256)
+
+Get all account orders; active, canceled, or filled.
+
+These orders will not be found:
+order status is CANCELED or EXPIRED, AND
+order has NO filled trade, AND
+created time + 7 days < current time
+Weight: 5
+
+Parameters:
+
+Name	Type	Mandatory	Description
+symbol	STRING	YES	
+orderId	LONG	NO	
+startTime	LONG	NO	
+endTime	LONG	NO	
+limit	INT	NO	Default 500; max 1000.
+recvWindow	LONG	NO	
+timestamp	LONG	YES	
+Notes:
+
+If orderId is set, it will get orders >= that orderId. Otherwise most recent orders are returned.
+The query time period must be less then 7 days( default as the recent 7 days).
+
+
+New Order (TRADE)
+Response:
+
+{
+    "clientOrderId": "testOrder",
+    "cumQty": "0",
+    "cumQuote": "0",
+    "executedQty": "0",
+    "orderId": 22542179,
+    "avgPrice": "0.00000",
+    "origQty": "10",
+    "price": "0",
+    "reduceOnly": false,
+    "side": "BUY",
+    "positionSide": "SHORT",
+    "status": "NEW",
+    "stopPrice": "9300",        // please ignore when order type is TRAILING_STOP_MARKET
+    "closePosition": false,   // if Close-All
+    "symbol": "BTCUSDT",
+    "timeInForce": "GTC",
+    "type": "TRAILING_STOP_MARKET",
+    "origType": "TRAILING_STOP_MARKET",
+    "activatePrice": "9020",    // activation price, only return with TRAILING_STOP_MARKET order
+    "priceRate": "0.3",         // callback rate, only return with TRAILING_STOP_MARKET order
+    "updateTime": 1566818724722,
+    "workingType": "CONTRACT_PRICE",
+    "priceProtect": false            // if conditional order trigger is protected   
+}
+POST /fapi/v1/order (HMAC SHA256)
+
+Send in a new order.
+
+Weight: 1
+
+Parameters:
+
+Name	Type	Mandatory	Description
+symbol	STRING	YES	
+side	ENUM	YES	
+positionSide	ENUM	NO	Default BOTH for One-way Mode ; LONG or SHORT for Hedge Mode. It must be sent in Hedge Mode.
+type	ENUM	YES	
+timeInForce	ENUM	NO	
+quantity	DECIMAL	NO	Cannot be sent with closePosition=true(Close-All)
+reduceOnly	STRING	NO	"true" or "false". default "false". Cannot be sent in Hedge Mode; cannot be sent with closePosition=true
+price	DECIMAL	NO	
+newClientOrderId	STRING	NO	A unique id among open orders. Automatically generated if not sent. Can only be string following the rule: ^[\.A-Z\:/a-z0-9_-]{1,36}$
+stopPrice	DECIMAL	NO	Used with STOP/STOP_MARKET or TAKE_PROFIT/TAKE_PROFIT_MARKET orders.
+closePosition	STRING	NO	true, false；Close-All，used with STOP_MARKET or TAKE_PROFIT_MARKET.
+activationPrice	DECIMAL	NO	Used with TRAILING_STOP_MARKET orders, default as the latest price(supporting different workingType)
+callbackRate	DECIMAL	NO	Used with TRAILING_STOP_MARKET orders, min 0.1, max 5 where 1 for 1%
+workingType	ENUM	NO	stopPrice triggered by: "MARK_PRICE", "CONTRACT_PRICE". Default "CONTRACT_PRICE"
+priceProtect	STRING	NO	"TRUE" or "FALSE", default "FALSE". Used with STOP/STOP_MARKET or TAKE_PROFIT/TAKE_PROFIT_MARKET orders.
+newOrderRespType	ENUM	NO	"ACK", "RESULT", default "ACK"
+recvWindow	LONG	NO	
+timestamp	LONG	YES	
+Additional mandatory parameters based on type:
+
+Type	Additional mandatory parameters
+LIMIT	timeInForce, quantity, price
+MARKET	quantity
+STOP/TAKE_PROFIT	quantity, price, stopPrice
+STOP_MARKET/TAKE_PROFIT_MARKET	stopPrice
+TRAILING_STOP_MARKET	callbackRate
+
 /fapi/v2/account
 
 GET /fapi/v2/balance (HMAC SHA256)
