ba878de4e42d86fa1ab63088763909a228f7f516
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-11 04:54:47 -0300

    Update data.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -401,7 +401,8 @@ async function data(request, response){
         stoch1w: stochRsi1w[stochRsi1w.length-1],
         stoch1wprev: stochRsi1w[stochRsi1w.length-2],
         
-        //openorders: openOrders
+        openorders: openOrders,
+        carteira: positions
 
     };
     
