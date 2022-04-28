d5738b87ecf617e5fda2a7a8aa1a4cbff7e4af5c
IsraelOliv <53785428+IsraelOliv@users.noreply.github.com>
2022-04-27 21:16:08 -0300

    Update data.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -686,7 +686,7 @@ async function calcOpenPosition(timestamp, sig){
     const dif1d = objSendcalc.stoch1d.k - objSendcalc.stoch1d.d;
     const dif1w = objSendcalc.stoch1w.k - objSendcalc.stoch1w.d;
 
-    if (sig.rsi3m >= 1 && dif1m > 0 && objSendcalc.stoch1m.k < 50 && flag == ""){      
+    if (sig.rsi3m >= 1 && dif1m > 0 && flag == ""){      
         // 1mC
         
         let orderBuy = await api.newOrderBuy(timestamp);
@@ -722,7 +722,7 @@ async function calcOpenPosition(timestamp, sig){
         await objSendcalc.positions.filter(b => b.symbol === 'BTCUSDT').set(obj); // || b.asset === 'USDT');
    */
     
-    }else if (sig.rsi3m <= -1 && dif1m < 0 && objSendcalc.stoch1m.k > 50 && flag == ""){
+    }else if (sig.rsi3m <= -1 && dif1m < 0 && flag == ""){
         // 1mV
 
         let orderSell = await api.newOrderSell(timestamp);
